925c8ba43e5691515a36f0c38566c2a9ddf82fd0
Saif3256 <167188461+Saif3256@users.noreply.github.com>
2024-10-25 21:51:23 +0500

    Why us component css add

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Header from "./Components/Header";
-
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import Whyus from "./Components/Why_us";
+
 function App() {
   
 
   return (
     <div>
       <Header/>
-
       <Hero/>
       <About/>
+      <Whyus/>
     </div>
   );
 }

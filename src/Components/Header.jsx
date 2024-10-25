809d747c9b5673e5f101f4500af8b1b6f432c280
Saif3256 <167188461+Saif3256@users.noreply.github.com>
2024-10-25 20:47:12 +0500

    Header Component Add

---
+++ src/Components/Header.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header id="header" className="header fixed-top">
+      <div className="topbar d-flex align-items-center">
+        <div className="container d-flex justify-content-center justify-content-md-between">
+          <div className="contact-info d-flex align-items-center">
+            <i className="bi bi-envelope d-flex align-items-center">
+              <a href="mailto:contact@example.com">contact@example.com</a>
+            </i>
+            <i className="bi bi-phone d-flex align-items-center ms-4">
+              <span>+1 5589 55488 55</span>
+            </i>
+          </div>
+          <div className="languages d-none d-md-flex align-items-center">
+            <ul>
+              <li>En</li>
+              <li><a href="#">De</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="branding d-flex align-items-center">
+        <div className="container position-relative d-flex align-items-center justify-content-between">
+          <a href="index.html" className="logo d-flex align-items-center me-auto me-xl-0">
+            <h1 className="sitename">Restaurantly</h1>
+          </a>
+
+          <nav id="navmenu" className="navmenu">
+            <ul>
+              <li><a href="#hero" className="active">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#menu">Menu</a></li>
+              <li><a href="#specials">Specials</a></li>
+              <li><a href="#events">Events</a></li>
+              <li><a href="#chefs">Chefs</a></li>
+              <li><a href="#gallery">Gallery</a></li>
+              <li className="dropdown">
+                <a href="#"><span>Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Dropdown 1</a></li>
+                  <li className="dropdown">
+                    <a href="#"><span>Deep Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
+                    <ul>
+                      <li><a href="#">Deep Dropdown 1</a></li>
+                      <li><a href="#">Deep Dropdown 2</a></li>
+                      <li><a href="#">Deep Dropdown 3</a></li>
+                      <li><a href="#">Deep Dropdown 4</a></li>
+                      <li><a href="#">Deep Dropdown 5</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Dropdown 2</a></li>
+                  <li><a href="#">Dropdown 3</a></li>
+                  <li><a href="#">Dropdown 4</a></li>
+                </ul>
+              </li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+            <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
+
+          <a className="btn-book-a-table d-none d-xl-block" href="#book-a-table">Book a Table</a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

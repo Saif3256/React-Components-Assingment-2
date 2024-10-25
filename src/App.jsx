b91f456a1e15fc9020b747ab035ad23c53e12119
Saif3256 <167188461+Saif3256@users.noreply.github.com>
2024-10-25 21:02:09 +0500

    Include Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import React from "react";
 import Header from "./Components/Header";
 
+import Hero from "./Components/Hero";
 
 function App() {
   
 
   return (
-    <Header/>
+    <div>
+      <Header/>
+
+      <Hero/>
+    </div>
   );
 }
 

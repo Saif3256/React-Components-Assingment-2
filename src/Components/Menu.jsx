5851249a5fc38d10727d96a93138775c6c64bfd0
Saif3256 <167188461+Saif3256@users.noreply.github.com>
2024-11-02 13:19:09 +0500

    Update testimonail and menu components

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import '../styles/Menu.css';
+import '../styles/Menu.css'; // Import your CSS file for styling
+import lobsterBisque from '../assets/img/menu/lobster-bisque.jpg'; // Import menu images
+import breadBarrel from '../assets/img/menu/bread-barrel.jpg';
+import crabCake from '../assets/img/menu/cake.jpg';
+import caesar from '../assets/img/menu/caesar.jpg';
+import tuscanGrilled from '../assets/img/menu/tuscan-grilled.jpg';
+import mozzarella from '../assets/img/menu/mozzarella.jpg';
+import greekSalad from '../assets/img/menu/greek-salad.jpg';
+import spinachSalad from '../assets/img/menu/spinach-salad.jpg';
+import lobsterRoll from '../assets/img/menu/lobster-roll.jpg';
 
 const Menu = () => {
   return (
     <section id="menu" className="menu section">
-     
+      {/* Section Title */}
       <div className="container section-title" data-aos="fade-up">
         <h2>Menu</h2>
         <p>Check Our Tasty Menu</p>
       </div>
-      
+      {/* End Section Title */}
+
       <div className="container isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
         <div className="row" data-aos="fade-up" data-aos-delay="100">
           <div className="col-lg-12 d-flex justify-content-center">
@@ -21,108 +31,91 @@ const Menu = () => {
             </ul>
           </div>
         </div>
-       
+        {/* Menu Filters */}
+
         <div className="row isotope-container" data-aos="fade-up" data-aos-delay="200">
-          <div className="col-lg-6 menu-item isotope-item filter-starters">
-            <img src="../assets/img/menu/lobster-bisque.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Lobster Bisque</a><span>$5.95</span>
-            </div>
-            <div className="menu-ingredients">
-              Lorem, deren, trataro, filede, nerada
-            </div>
-          </div>
-         
-
-          <div className="col-lg-6 menu-item isotope-item filter-specialty">
-            <img src="../assets/img/menu/bread-barrel.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Bread Barrel</a><span>$6.95</span>
-            </div>
-            <div className="menu-ingredients">
-              Lorem, deren, trataro, filede, nerada
-            </div>
-          </div>
-         
-
-          <div className="col-lg-6 menu-item isotope-item filter-starters">
-            <img src="../assets/img/menu/cake.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Crab Cake</a><span>$7.95</span>
-            </div>
-            <div className="menu-ingredients">
-              A delicate crab cake served on a toasted roll with lettuce and tartar sauce
-            </div>
-          </div>
-         
-          <div className="col-lg-6 menu-item isotope-item filter-salads">
-            <img src="../assets/img/menu/caesar.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Caesar Selections</a><span>$8.95</span>
-            </div>
-            <div className="menu-ingredients">
-              Lorem, deren, trataro, filede, nerada
-            </div>
-          </div>
-         
-
-          <div className="col-lg-6 menu-item isotope-item filter-specialty">
-            <img src="../assets/img/menu/tuscan-grilled.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Tuscan Grilled</a><span>$9.95</span>
-            </div>
-            <div className="menu-ingredients">
-              Grilled chicken with provolone, artichoke hearts, and roasted red pesto
-            </div>
-          </div>
-        
-          <div className="col-lg-6 menu-item isotope-item filter-starters">
-            <img src="../assets/img/menu/mozzarella.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Mozzarella Stick</a><span>$4.95</span>
-            </div>
-            <div className="menu-ingredients">
-              Lorem, deren, trataro, filede, nerada
-            </div>
-          </div>
-          {/* Menu Item */}
-
-          <div className="col-lg-6 menu-item isotope-item filter-salads">
-            <img src="../assets/img/menu/greek-salad.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Greek Salad</a><span>$9.95</span>
-            </div>
-            <div className="menu-ingredients">
-              Fresh spinach, crisp romaine, tomatoes, and Greek olives
-            </div>
-          </div>
-          {/* Menu Item */}
-
-          <div className="col-lg-6 menu-item isotope-item filter-salads">
-            <img src="../assets/img/menu/spinach-salad.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Spinach Salad</a><span>$9.95</span>
-            </div>
-            <div className="menu-ingredients">
-              Fresh spinach with mushrooms, hard boiled egg, and warm bacon vinaigrette
-            </div>
-          </div>
-          {/* Menu Item */}
-
-          <div className="col-lg-6 menu-item isotope-item filter-specialty">
-            <img src="../assets/img/menu/lobster-roll.jpg" className="menu-img" alt="" />
-            <div className="menu-content">
-              <a href="#">Lobster Roll</a><span>$12.95</span>
-            </div>
-            <div className="menu-ingredients">
-              Plump lobster meat, mayo and crisp lettuce on a toasted bulky roll
-            </div>
-          </div>
-          {/* Menu Item */}
+          <MenuItem 
+            image={lobsterBisque} 
+            title="Lobster Bisque" 
+            price="$5.95" 
+            ingredients="Lorem, deren, trataro, filede, nerada"
+            filterClass="filter-starters" 
+          />
+          <MenuItem 
+            image={breadBarrel} 
+            title="Bread Barrel" 
+            price="$6.95" 
+            ingredients="Lorem, deren, trataro, filede, nerada"
+            filterClass="filter-specialty" 
+          />
+          <MenuItem 
+            image={crabCake} 
+            title="Crab Cake" 
+            price="$7.95" 
+            ingredients="A delicate crab cake served on a toasted roll with lettuce and tartar sauce"
+            filterClass="filter-starters" 
+          />
+          <MenuItem 
+            image={caesar} 
+            title="Caesar Selections" 
+            price="$8.95" 
+            ingredients="Lorem, deren, trataro, filede, nerada"
+            filterClass="filter-salads" 
+          />
+          <MenuItem 
+            image={tuscanGrilled} 
+            title="Tuscan Grilled" 
+            price="$9.95" 
+            ingredients="Grilled chicken with provolone, artichoke hearts, and roasted red pesto"
+            filterClass="filter-specialty" 
+          />
+          <MenuItem 
+            image={mozzarella} 
+            title="Mozzarella Stick" 
+            price="$4.95" 
+            ingredients="Lorem, deren, trataro, filede, nerada"
+            filterClass="filter-starters" 
+          />
+          <MenuItem 
+            image={greekSalad} 
+            title="Greek Salad" 
+            price="$9.95" 
+            ingredients="Fresh spinach, crisp romaine, tomatoes, and Greek olives"
+            filterClass="filter-salads" 
+          />
+          <MenuItem 
+            image={spinachSalad} 
+            title="Spinach Salad" 
+            price="$9.95" 
+            ingredients="Fresh spinach with mushrooms, hard boiled egg, and warm bacon vinaigrette"
+            filterClass="filter-salads" 
+          />
+          <MenuItem 
+            image={lobsterRoll} 
+            title="Lobster Roll" 
+            price="$12.95" 
+            ingredients="Plump lobster meat, mayo and crisp lettuce on a toasted bulky roll"
+            filterClass="filter-specialty" 
+          />
         </div>
         {/* Menu Container */}
       </div>
     </section>
+  );
+};
+
+// MenuItem component to handle individual menu items
+const MenuItem = ({ image, title, price, ingredients, filterClass }) => {
+  return (
+    <div className={`col-lg-6 menu-item isotope-item ${filterClass}`}>
+      <img src={image} className="menu-img" alt={title} />
+      <div className="menu-content">
+        <a href="#">{title}</a><span>{price}</span>
+      </div>
+      <div className="menu-ingredients">
+        {ingredients}
+      </div>
+    </div>
   );
 };
 
--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import '../styles/Testimonials.css'; // Import your CSS file for styling
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'; // Import Swiper styles
+
+// Importing testimonial images
 import testimonial1 from '../assets/img/testimonials/testimonials-1.jpg';
 import testimonial2 from '../assets/img/testimonials/testimonials-2.jpg';
 import testimonial3 from '../assets/img/testimonials/testimonials-3.jpg';
 import testimonial4 from '../assets/img/testimonials/testimonials-4.jpg';
 import testimonial5 from '../assets/img/testimonials/testimonials-5.jpg';
-import '../styles/Testimonials.css'; // Adjust the path as necessary
 
 const Testimonials = () => {
+  const testimonials = [
+    {
+      image: testimonial1,
+      name: 'Saul Goodman',
+      position: 'Ceo & Founder',
+      text: 'Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.'
+    },
+    {
+      image: testimonial2,
+      name: 'Sara Wilsson',
+      position: 'Designer',
+      text: 'Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.'
+    },
+    {
+      image: testimonial3,
+      name: 'Jena Karlis',
+      position: 'Store Owner',
+      text: 'Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.'
+    },
+    {
+      image: testimonial4,
+      name: 'Matt Brandon',
+      position: 'Freelancer',
+      text: 'Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.'
+    },
+    {
+      image: testimonial5,
+      name: 'John Larson',
+      position: 'Entrepreneur',
+      text: 'Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.'
+    }
+  ];
+
   return (
     <section id="testimonials" className="testimonials section">
       {/* Section Title */}
@@ -17,153 +54,52 @@ const Testimonials = () => {
       {/* End Section Title */}
 
       <div className="container" data-aos="fade-up" data-aos-delay="100">
-        <div
-          className="swiper init-swiper"
-          data-speed="600"
-          data-delay="5000"
-          data-breakpoints='{"320": {"slidesPerView": 1, "spaceBetween": 40}, "1200": {"slidesPerView": 3, "spaceBetween": 40}}'
+        <Swiper
+          loop={true}
+          speed={600}
+          autoplay={{ delay: 5000 }}
+          slidesPerView={'auto'}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 40,
+            },
+            1200: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }}
         >
-          <script type="application/json" className="swiper-config">
-            {JSON.stringify({
-              loop: true,
-              speed: 600,
-              autoplay: {
-                delay: 5000,
-              },
-              slidesPerView: 'auto',
-              pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-                clickable: true,
-              },
-              breakpoints: {
-                320: {
-                  slidesPerView: 1,
-                  spaceBetween: 40,
-                },
-                1200: {
-                  slidesPerView: 3,
-                  spaceBetween: 20,
-                },
-              },
-            })}
-          </script>
-          <div className="swiper-wrapper">
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <p>
-                  <i className="bi bi-quote quote-icon-left"></i>
-                  <span>
-                    Proin iaculis purus consequat sem cure digni ssim donec
-                    porttitora entum suscipit rhoncus. Accusantium quam,
-                    ultricies eget id, aliquam eget nibh et. Maecen aliquam,
-                    risus at semper.
-                  </span>
-                  <i className="bi bi-quote quote-icon-right"></i>
-                </p>
-                <img
-                  src={testimonial1}
-                  className="testimonial-img"
-                  alt="Saul Goodman"
-                />
-                <h3>Saul Goodman</h3>
-                <h4>Ceo & Founder</h4>
-              </div>
-            </div>
-            {/* End testimonial item */}
-
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <p>
-                  <i className="bi bi-quote quote-icon-left"></i>
-                  <span>
-                    Export tempor illum tamen malis malis eram quae irure esse
-                    labore quem cillum quid malis quorum velit fore eram velit
-                    sunt aliqua noster fugiat irure amet legam anim culpa.
-                  </span>
-                  <i className="bi bi-quote quote-icon-right"></i>
-                </p>
-                <img
-                  src={testimonial2}
-                  className="testimonial-img"
-                  alt="Sara Wilsson"
-                />
-                <h3>Sara Wilsson</h3>
-                <h4>Designer</h4>
-              </div>
-            </div>
-            {/* End testimonial item */}
-
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <p>
-                  <i className="bi bi-quote quote-icon-left"></i>
-                  <span>
-                    Enim nisi quem export duis labore cillum quae magna enim
-                    sint quorum nulla quem veniam duis minim tempor labore quem
-                    eram duis noster aute amet eram fore quis sint minim.
-                  </span>
-                  <i className="bi bi-quote quote-icon-right"></i>
-                </p>
-                <img
-                  src={testimonial3}
-                  className="testimonial-img"
-                  alt="Jena Karlis"
-                />
-                <h3>Jena Karlis</h3>
-                <h4>Store Owner</h4>
-              </div>
-            </div>
-            {/* End testimonial item */}
-
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <p>
-                  <i className="bi bi-quote quote-icon-left"></i>
-                  <span>
-                    Fugiat enim eram quae cillum dolore dolor amet nulla culpa
-                    multos export minim fugiat dolor enim duis veniam ipsum anim
-                    magna sunt elit fore quem dolore labore illum veniam.
-                  </span>
-                  <i className="bi bi-quote quote-icon-right"></i>
-                </p>
-                <img
-                  src={testimonial4}
-                  className="testimonial-img"
-                  alt="Matt Brandon"
-                />
-                <h3>Matt Brandon</h3>
-                <h4>Freelancer</h4>
-              </div>
-            </div>
-            {/* End testimonial item */}
-
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <p>
-                  <i className="bi bi-quote quote-icon-left"></i>
-                  <span>
-                    Quis quorum aliqua sint quem legam fore sunt eram irure
-                    aliqua veniam tempor noster veniam sunt culpa nulla illum
-                    cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                  </span>
-                  <i className="bi bi-quote quote-icon-right"></i>
-                </p>
-                <img
-                  src={testimonial5}
-                  className="testimonial-img"
-                  alt="John Larson"
-                />
-                <h3>John Larson</h3>
-                <h4>Entrepreneur</h4>
-              </div>
-            </div>
-            {/* End testimonial item */}
-          </div>
-          <div className="swiper-pagination"></div>
-        </div>
+          {testimonials.map((testimonial, index) => (
+            <SwiperSlide key={index}>
+              <TestimonialItem 
+                image={testimonial.image} 
+                name={testimonial.name} 
+                position={testimonial.position} 
+                text={testimonial.text} 
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
+  );
+};
+
+// TestimonialItem component to handle individual testimonials
+const TestimonialItem = ({ image, name, position, text }) => {
+  return (
+    <div className="testimonial-item">
+      <p>
+        <i className="bi bi-quote quote-icon-left"></i>
+        <span>{text}</span>
+        <i className="bi bi-quote quote-icon-right"></i>
+      </p>
+      <img src={image} className="testimonial-img" alt={name} />
+      <h3>{name}</h3>
+      <h4>{position}</h4>
+    </div>
   );
 };
 
